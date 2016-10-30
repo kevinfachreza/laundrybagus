178d52f00fb83f3d87e7f38c686ad6a9174230eb
HAMKA\A46CB <hamkaaminullah@yahoo.com>
2016-10-30 10:52:18 +0700

    ngene ta?

---
+++ application/views/home/index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="<?php echo base_url()?>assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url()?>assets/css/style.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">HOME</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li><a href="#">PAKET LAUNDRY <span class="sr-only">(current)</span></a></li>
+            <li><a href="#">CHEMICAL</a></li>
+            <li><a href="#">HUBUNGI KAMI</a></li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+
+    <div id="BukaUsaha" class="container-fluid padding-0">
+        BUKA USAHA LAUNDRY<br>
+        ITU GAK RIBET
+    </div>
+
+    <div id="SiapaLB" class="container">
+      SIAPA LAUNDRY BAGUS ?
+      <div class="container content-SiapaLB">
+        LaundryBagus bergerak di bidang jasa pembuatan konsep usaha laundry sesuai budget anda,
+        harga paket sudah termasuk training karyawan dan supervisi owner. Laundrybagus
+        mempunyai SOP yang telah teruji, tanpa royalty fee, tanpa pinalty dan tanpa franchise fee.
+        Artinya KEUNTUNGAN ANDA 100% MILIK ANDA, kami hanya membantu membuatkan peluang usaha laundry 
+        sesuai dengan konsep dan budget anda.
+      </div>
+    </div>
+
+    <div id="KapanSaat" class="container-fluid padding-0">
+      KAPAN SAAT YANG TEPAT <br>UNTUK BUKA LAUNDRY ?
+      <div id="Sekarang">SEKARANG</div>
+      <div class="container content-KapanSaat">
+        Karena bisa jadi saat anda browsing peluang usaha laundry
+        ada ribuan orang juga yang sedang melakukan hal yang sama,
+        bagaimana kalau ternyata tetangga depan rumah anda buka duluan ???
+      </div>
+    </div>
+
+    <div id="HanyaLB" class="container-fluid padding-0"> 
+      HANYA DI LAUNDRY BAGUS
+      <div id="content-HanyaLB" class="row padding-lr">
+        <div class="col-md-3"><span class="glyphicons glyphicons-shop" aria-hidden="true"></span><br>
+          <div class="bold">PEMBELIAN DENGAN<br>CICILAN RINGAN<br></div>
+          <br>Cicilan 0% dan berbagai kemudahan lainnya dapat anda dapatkan hanya di laundry bagus
+        </div>
+        <div class="col-md-3"><br>
+          <div class="bold">PENGIRIMAN GRATIS<br>KEMANA MANA</div>
+          <br>Tidak perlu repot repot mengurusi pengiriman ke tempat usaha anda karena akan kami bantu untuk pengurusan barang 
+        </div>
+        <div class="col-md-3"><br>
+          <div class="bold">JAMINAN LAYANAN<br>TOP MARKOTOP</div>
+          <br>Dengan kualitas mesin laundry yang sangat baik tentu saja akan membuat bisnis anda akan menjadi baik pula
+        </div>
+        <div class="col-md-3"><br>
+          <div class="bold">KONSULTASI BISNIS<br>DAN SEMINAR LAUNDRY</div>
+          <br>Jangan khawatir karena kami akan membantu anda dengan bisnis anda. Juga kami akan memberikan anda tips-tips untuk menjalankan usaha anda
+        </div>
+      </div>
+    </div>
+
+    <div id="PaketLB" class="container-fluid padding-0">
+      <div class="row padding-lr">
+        <div class="col-md-4">
+          PAKET USAHA<br>
+          LAUNDRY BAGUS
+        </div>
+        <div class="col-md-4 content-PaketLB">
+          <div class="bold med">PAKET LAUNDRY OUTLET</div><br>
+          Mulai dari 34 juta anda bisa memulai bisnis laundry dengan mudah disertai bonus-bonus yang menarik<br><br>
+          <button type="button" class="btn btn-primary red" data-toggle="button" aria-pressed="false" autocomplete="off">
+            KLIK DISINI
+          </button>
+        </div>
+        <div class="col-md-4 content-PaketLB">
+          <div class="bold med">PAKET LAUNDRY RUMAHAN</div><br>
+          Mulai dari 12 juta anda bisa memulai bisnis laundry di rumah anda sendiri<br><br><br>
+          <button type="button" class="btn btn-primary red" data-toggle="button" aria-pressed="false" autocomplete="off">
+            KLIK DISINI
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div id="MengapaLB" class="container-fluid padding-0">
+      MENGAPA HARUS MENGGUNAKAN CHEMICAL BERKUALITAS ?<br><br>
+      <div class="container content-MengapaLB">
+        Secara sepintas, hasil kinerja deterjen tidak begitu terlihat pada pakaian yang telah selesai menjalani proses pencucian
+        Namun, jika kita amati dengan seksama, kita akan mengetahui kualitas deterjen yang kita gunakan.
+        Kita harus tahu apakah deterjen itu membuat pakaian menjadi kusam atau
+        mampu menjaga kecemerlangan warna kain untuk menjaga kualitas hasil cucian laundry.<br><br>
+        <button type="button" class="btn btn-primary red" data-toggle="button" aria-pressed="false" autocomplete="off">
+          INFO LEBIH LANJUT
+        </button>
+      </div> 
+    </div>
+
+    <div id="SiapLB" class="container-fluid padding-0">
+      LAUNDRY BAGUS SIAP MELAYANI ANDA
+    </div>
+
+    <div id="ContactLB" class="container-fluid padding-0 bold">
+      <div>LAUNDRY BAGUS<br></div>
+        0856 4207 1199<br>
+        0852 9088 5711<br>
+        mesinlaundrysolo@gmail.com<br>
+        Jalan Kapten Mulyadi no 264 Solo
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="<?php echo base_url()?>assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
