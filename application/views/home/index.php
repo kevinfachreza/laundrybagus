21550736e25aaf2d39879ea1ec7f2ab5a3507b65
HAMKA\A46CB <hamkaaminullah@yahoo.com>
2016-11-02 02:05:36 +0700

    nyoh

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -43,14 +43,16 @@
       </div><!-- /.container-fluid -->
     </nav>
 
-    <div id="BukaUsaha" class="container-fluid padding-0">
-        BUKA USAHA LAUNDRY<br>
-        ITU GAK RIBET
+    <div id="BukaUsaha" class="container-fluid">
+      BUKA USAHA LAUNDRY<br>
+      ITU GAK RIBET
     </div>
 
-    <div id="SiapaLB" class="container">
-      SIAPA LAUNDRY BAGUS ?
-      <div class="container content-SiapaLB">
+    <div id="SiapaLB" class="container-fluid">
+      <div class="header-LB">
+        SIAPA LAUNDRY BAGUS ?
+      </div>
+      <div class="container content-LB col-md-6 col-md-offset-3">
         LaundryBagus bergerak di bidang jasa pembuatan konsep usaha laundry sesuai budget anda,
         harga paket sudah termasuk training karyawan dan supervisi owner. Laundrybagus
         mempunyai SOP yang telah teruji, tanpa royalty fee, tanpa pinalty dan tanpa franchise fee.
@@ -59,19 +61,23 @@
       </div>
     </div>
 
-    <div id="KapanSaat" class="container-fluid padding-0">
-      KAPAN SAAT YANG TEPAT <br>UNTUK BUKA LAUNDRY ?
+    <div id="KapanSaat" class="container-fluid">
+      <div class="header-LB">
+        KAPAN SAAT YANG TEPAT UNTUK BUKA LAUNDRY ?
+      </div>
       <div id="Sekarang">SEKARANG</div>
-      <div class="container content-KapanSaat">
+      <div class="container content-LB col-md-6 col-md-offset-3">
         Karena bisa jadi saat anda browsing peluang usaha laundry
         ada ribuan orang juga yang sedang melakukan hal yang sama,
         bagaimana kalau ternyata tetangga depan rumah anda buka duluan ???
       </div>
     </div>
 
-    <div id="HanyaLB" class="container-fluid padding-0"> 
-      HANYA DI LAUNDRY BAGUS
-      <div id="content-HanyaLB" class="row padding-lr">
+    <div id="HanyaLB" class="container-fluid"> 
+      <div class="header-LB">
+        HANYA DI LAUNDRY BAGUS
+      </div>
+      <div id="content-HanyaLB" class="col-md-8 col-md-offset-2">
         <div class="col-md-3"><span class="glyphicons glyphicons-shop" aria-hidden="true"></span><br>
           <div class="bold">PEMBELIAN DENGAN<br>CICILAN RINGAN<br></div>
           <br>Cicilan 0% dan berbagai kemudahan lainnya dapat anda dapatkan hanya di laundry bagus
@@ -91,52 +97,75 @@
       </div>
     </div>
 
-    <div id="PaketLB" class="container-fluid padding-0">
-      <div class="row padding-lr">
+    <div id="PaketLB" class="container-fluid">
+      <div class="col-md-9 col-md-offset-2">
+        <div class="col-md-4 header-PaketLB row-height">
+          <div class="col-md-10">
+            PAKET USAHA LAUNDRY BAGUS
+          </div>
+        </div>
         <div class="col-md-4">
-          PAKET USAHA<br>
-          LAUNDRY BAGUS
+          <div class="col-md-12 content-PaketLB">
+            <div class="bold med space col-md-10 col-md-offset-1">
+              PAKET LAUNDRY OUTLET
+            </div>
+            <div class="content-PaketLB-text">
+              Mulai dari 34 juta anda bisa memulai bisnis laundry dengan mudah disertai bonus-bonus yang menarik
+            </div>
+            <div class="content-PaketLB-button">
+              <button type="button" class="btn btn-primary red" data-toggle="button" aria-pressed="false" autocomplete="off">
+                KLIK DISINI
+              </button>
+            </div>
+          </div>
         </div>
-        <div class="col-md-4 content-PaketLB">
-          <div class="bold med">PAKET LAUNDRY OUTLET</div><br>
-          Mulai dari 34 juta anda bisa memulai bisnis laundry dengan mudah disertai bonus-bonus yang menarik<br><br>
-          <button type="button" class="btn btn-primary red" data-toggle="button" aria-pressed="false" autocomplete="off">
-            KLIK DISINI
-          </button>
-        </div>
-        <div class="col-md-4 content-PaketLB">
-          <div class="bold med">PAKET LAUNDRY RUMAHAN</div><br>
-          Mulai dari 12 juta anda bisa memulai bisnis laundry di rumah anda sendiri<br><br><br>
-          <button type="button" class="btn btn-primary red" data-toggle="button" aria-pressed="false" autocomplete="off">
-            KLIK DISINI
-          </button>
+        <div class="col-md-4">
+          <div class="col-md-12 content-PaketLB">
+            <div class="bold med space col-md-10 col-md-offset-1">
+              PAKET LAUNDRY RUMAHAN
+            </div>
+            <div class="content-PaketLB-text">
+              Mulai dari 12 juta anda bisa memulai bisnis laundry di rumah anda sendiri
+            </div>
+            <div class="content-PaketLB-button">
+              <button type="button" class="btn btn-primary red" data-toggle="button" aria-pressed="false" autocomplete="off">
+                KLIK DISINI
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
 
-    <div id="MengapaLB" class="container-fluid padding-0">
-      MENGAPA HARUS MENGGUNAKAN CHEMICAL BERKUALITAS ?<br><br>
-      <div class="container content-MengapaLB">
+    <div id="MengapaLB" class="container-fluid">
+      <div class="header-LB col-md-6 col-md-offset-3">
+        MENGAPA HARUS MENGGUNAKAN CHEMICAL BERKUALITAS ?
+      </div>
+      <div class="container content-LB col-md-6 col-md-offset-3">
         Secara sepintas, hasil kinerja deterjen tidak begitu terlihat pada pakaian yang telah selesai menjalani proses pencucian
         Namun, jika kita amati dengan seksama, kita akan mengetahui kualitas deterjen yang kita gunakan.
         Kita harus tahu apakah deterjen itu membuat pakaian menjadi kusam atau
-        mampu menjaga kecemerlangan warna kain untuk menjaga kualitas hasil cucian laundry.<br><br>
-        <button type="button" class="btn btn-primary red" data-toggle="button" aria-pressed="false" autocomplete="off">
-          INFO LEBIH LANJUT
-        </button>
+        mampu menjaga kecemerlangan warna kain untuk menjaga kualitas hasil cucian laundry.
+        <div class="content-LB-button">
+          <button type="button" class="btn btn-primary red" data-toggle="button" aria-pressed="false" autocomplete="off">
+            INFO LEBIH LANJUT
+          </button>
+        </div>
       </div> 
     </div>
 
-    <div id="SiapLB" class="container-fluid padding-0">
+    <div id="SiapLB" class="container-fluid">
       LAUNDRY BAGUS SIAP MELAYANI ANDA
     </div>
 
-    <div id="ContactLB" class="container-fluid padding-0 bold">
-      <div>LAUNDRY BAGUS<br></div>
+    <div id="ContactLB" class="container-fluid bold">
+      LAUNDRY BAGUS<br>
+      <div class="left">
         0856 4207 1199<br>
         0852 9088 5711<br>
         mesinlaundrysolo@gmail.com<br>
         Jalan Kapten Mulyadi no 264 Solo
+      </div>
     </div>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
