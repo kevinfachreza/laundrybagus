568e40f7872add62d3be63022abd3857c51f87cc
DESKTOP-HTOPP7T\Kevin <kevinfachreza@yahoo.com>
2016-11-03 00:46:14 +0700

    homefix

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -19,7 +19,7 @@
     <![endif]-->
   </head>
   <body>
-    <nav class="navbar navbar-default">
+    <nav class="nav navbar navbar-default navbar-fixed-top navbar-mode-2" id="navbar">
       <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
@@ -34,7 +34,7 @@
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav">
+          <ul class="nav navbar-nav navbar-right">
             <li><a href="#">PAKET LAUNDRY <span class="sr-only">(current)</span></a></li>
             <li><a href="#">CHEMICAL</a></li>
             <li><a href="#">HUBUNGI KAMI</a></li>
@@ -44,104 +44,87 @@
     </nav>
 
     <div id="BukaUsaha" class="container-fluid">
-      BUKA USAHA LAUNDRY<br>
-      ITU GAK RIBET
+      <div class="header-maintext">Buka Usaha Laundry Itu<br>
+      Gak Ribet</div>
+	  <div class="header-subtext">Kemudahan Laundry Untuk Semua Orang</div>
     </div>
 
     <div id="SiapaLB" class="container-fluid">
       <div class="header-LB">
-        SIAPA LAUNDRY BAGUS ?
+        Semakin Mudah Untuk Membuka Usaha<br>
+		Bersama LaundryBagus
       </div>
-      <div class="container content-LB col-md-6 col-md-offset-3">
-        LaundryBagus bergerak di bidang jasa pembuatan konsep usaha laundry sesuai budget anda,
-        harga paket sudah termasuk training karyawan dan supervisi owner. Laundrybagus
-        mempunyai SOP yang telah teruji, tanpa royalty fee, tanpa pinalty dan tanpa franchise fee.
+      <div class="container content-LB col-md-8 col-md-offset-2">
+        <div class="content-LB">LaundryBagus bergerak di bidang jasa pembuatan konsep usaha laundry sesuai budget anda,
+        harga paket sudah termasuk training karyawan dan supervisi owner. </div>
+		
+		<div class="content-LB">Laundrybagus mempunyai SOP yang telah teruji, tanpa royalty fee, tanpa pinalty dan tanpa franchise fee.
         Artinya KEUNTUNGAN ANDA 100% MILIK ANDA, kami hanya membantu membuatkan peluang usaha laundry 
-        sesuai dengan konsep dan budget anda.
+        sesuai dengan konsep dan budget anda.</div>
       </div>
     </div>
 
     <div id="KapanSaat" class="container-fluid">
-      <div class="header-LB">
-        KAPAN SAAT YANG TEPAT UNTUK BUKA LAUNDRY ?
-      </div>
       <div id="Sekarang">SEKARANG</div>
-      <div class="container content-LB col-md-6 col-md-offset-3">
-        Karena bisa jadi saat anda browsing peluang usaha laundry
-        ada ribuan orang juga yang sedang melakukan hal yang sama,
-        bagaimana kalau ternyata tetangga depan rumah anda buka duluan ???
+      <div class="container content-LB content-KapanSaat col-md-6 col-md-offset-3">
+        Adalah Saat Yang Tepat Untuk Membuat Bisnis Laundry
       </div>
     </div>
 
     <div id="HanyaLB" class="container-fluid"> 
       <div class="header-LB">
-        HANYA DI LAUNDRY BAGUS
+		Hanya di LaundryBagus
       </div>
-      <div id="content-HanyaLB" class="col-md-8 col-md-offset-2">
-        <div class="col-md-3"><span class="glyphicons glyphicons-shop" aria-hidden="true"></span><br>
-          <div class="bold">PEMBELIAN DENGAN<br>CICILAN RINGAN<br></div>
-          <br>Cicilan 0% dan berbagai kemudahan lainnya dapat anda dapatkan hanya di laundry bagus
+      <div id="content-HanyaLB" class="col-md-8 col-md-offset-2 ">
+        <div class="col-md-3  col-sm-3">
+          <div class="HanyaLb-Col-Header">PEMBELIAN DENGAN<br>CICILAN RINGAN<br></div>
+          <div class="HanyaLb-Col-Content">Cicilan 0% dan berbagai kemudahan lainnya dapat anda dapatkan hanya di laundry bagus</div>
         </div>
-        <div class="col-md-3"><br>
-          <div class="bold">PENGIRIMAN GRATIS<br>KEMANA MANA</div>
-          <br>Tidak perlu repot repot mengurusi pengiriman ke tempat usaha anda karena akan kami bantu untuk pengurusan barang 
+        <div class="col-md-3 col-sm-3">
+          <div class="HanyaLb-Col-Header">PENGIRIMAN GRATIS<br>KEMANA MANA</div>
+          <div class="HanyaLb-Col-Content">Tidak perlu repot repot mengurusi pengiriman ke tempat usaha anda karena akan kami bantu untuk pengurusan barang </div>
         </div>
-        <div class="col-md-3"><br>
-          <div class="bold">JAMINAN LAYANAN<br>TOP MARKOTOP</div>
-          <br>Dengan kualitas mesin laundry yang sangat baik tentu saja akan membuat bisnis anda akan menjadi baik pula
+        <div class="col-md-3 col-sm-3">
+          <div class="HanyaLb-Col-Header">JAMINAN LAYANAN<br>TOP MARKOTOP</div>
+          <div class="HanyaLb-Col-Content">Dengan kualitas mesin laundry yang sangat baik tentu saja akan membuat bisnis anda akan menjadi baik pula</div>
         </div>
-        <div class="col-md-3"><br>
-          <div class="bold">KONSULTASI BISNIS<br>DAN SEMINAR LAUNDRY</div>
-          <br>Jangan khawatir karena kami akan membantu anda dengan bisnis anda. Juga kami akan memberikan anda tips-tips untuk menjalankan usaha anda
+        <div class="col-md-3 col-sm-3">
+          <div class="HanyaLb-Col-Header">KONSULTASI BISNIS<br>DAN SEMINAR LAUNDRY</div>
+          <div class="HanyaLb-Col-Content">Jangan khawatir karena kami akan membantu anda dengan bisnis anda. Juga kami akan memberikan anda tips-tips untuk menjalankan usaha anda</div>
         </div>
       </div>
     </div>
-
+	
+	<hr class="LB-Divider">
+	
     <div id="PaketLB" class="container-fluid">
-      <div class="col-md-9 col-md-offset-2">
-        <div class="col-md-4 header-PaketLB row-height">
-          <div class="col-md-10">
-            PAKET USAHA LAUNDRY BAGUS
-          </div>
+      <div class="header-LB text-center">
+		Paket Franchise LaundryBagus
+      </div>
+      <div id="content-PaketLB" class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
+        <div class="col-md-4 col-sm-4">
+			<div class="PaketLB-Col-Image"><img src="http://bukalaundry.co/images/paket/rumahan.png"></div>
+			<div class="PaketLB-Col-Header">KONSEP RUKO<br></div>
+			<div class="PaketLB-Col-Content">Cicilan 0% dan berbagai kemudahan lainnya dapat anda dapatkan hanya di laundry bagus</div>
         </div>
-        <div class="col-md-4">
-          <div class="col-md-12 content-PaketLB">
-            <div class="bold med space col-md-10 col-md-offset-1">
-              PAKET LAUNDRY OUTLET
-            </div>
-            <div class="content-PaketLB-text">
-              Mulai dari 34 juta anda bisa memulai bisnis laundry dengan mudah disertai bonus-bonus yang menarik
-            </div>
-            <div class="content-PaketLB-button">
-              <button type="button" class="btn btn-primary red" data-toggle="button" aria-pressed="false" autocomplete="off">
-                KLIK DISINI
-              </button>
-            </div>
-          </div>
+        <div class="col-md-4 col-sm-4">
+			<div class="PaketLB-Col-Image"><img src="http://bukalaundry.co/images/paket/rumahan.png"></div>
+			<div class="PaketLB-Col-Header">KONSEP RUMAHAN</div>
+			<div class="PaketLB-Col-Content">Tidak perlu repot repot mengurusi pengiriman ke tempat usaha anda karena akan kami bantu untuk pengurusan barang </div>
         </div>
-        <div class="col-md-4">
-          <div class="col-md-12 content-PaketLB">
-            <div class="bold med space col-md-10 col-md-offset-1">
-              PAKET LAUNDRY RUMAHAN
-            </div>
-            <div class="content-PaketLB-text">
-              Mulai dari 12 juta anda bisa memulai bisnis laundry di rumah anda sendiri
-            </div>
-            <div class="content-PaketLB-button">
-              <button type="button" class="btn btn-primary red" data-toggle="button" aria-pressed="false" autocomplete="off">
-                KLIK DISINI
-              </button>
-            </div>
-          </div>
+        <div class="col-md-4 col-sm-4">
+			<div class="PaketLB-Col-Image"><img src="http://bukalaundry.co/images/paket/rumahan.png"></div>
+			<div class="PaketLB-Col-Header">KONSEP OUTLET</div>
+			<div class="PaketLB-Col-Content">Dengan kualitas mesin laundry yang sangat baik tentu saja akan membuat bisnis anda akan menjadi baik pula</div>
         </div>
       </div>
     </div>
 
     <div id="MengapaLB" class="container-fluid">
-      <div class="header-LB col-md-6 col-md-offset-3">
-        MENGAPA HARUS MENGGUNAKAN CHEMICAL BERKUALITAS ?
+      <div class="header-LB col-md-12 ">
+        MENGAPA HARUS MENGGUNAKAN<br>CHEMICAL BERKUALITAS ?
       </div>
-      <div class="container content-LB col-md-6 col-md-offset-3">
+      <div class="container content-LB col-md-8 col-md-offset-2">
         Secara sepintas, hasil kinerja deterjen tidak begitu terlihat pada pakaian yang telah selesai menjalani proses pencucian
         Namun, jika kita amati dengan seksama, kita akan mengetahui kualitas deterjen yang kita gunakan.
         Kita harus tahu apakah deterjen itu membuat pakaian menjadi kusam atau
@@ -158,19 +141,53 @@
       LAUNDRY BAGUS SIAP MELAYANI ANDA
     </div>
 
-    <div id="ContactLB" class="container-fluid bold">
-      LAUNDRY BAGUS<br>
-      <div class="left">
-        0856 4207 1199<br>
-        0852 9088 5711<br>
-        mesinlaundrysolo@gmail.com<br>
-        Jalan Kapten Mulyadi no 264 Solo
-      </div>
-    </div>
+    <footer id="ContactLB" class="container-fluid bold">
+		<div class="footer-header">Hubungi Kami</div>
+		<div class="footer-list">
+			0856 4207 1199<br>
+			0852 9088 5711<br>
+			mesinlaundrysolo@gmail.com<br>
+			Jalan Kapten Mulyadi no 264 Solo
+		</div>
+    </footer>
+	<div class="col-md-12 footer-second text-center">
+		CopyRight - LaundryBagus
+	</div>
 
+	
+	
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="<?php echo base_url()?>assets/js/bootstrap.min.js"></script>
+	
+	<script>
+		$(document).ready(function(){       
+	   var scroll_start = 0;
+	   var startchange = $('.nav');
+	   var offset = startchange.offset();
+	   $(document).scroll(function() { 
+		  scroll_start = $(this).scrollTop();
+		  if(scroll_start > offset.top) {
+		   } else {
+		   }
+	   });
+	});
+	</script>
+
+	<script type="text/javascript">
+        $(window).scroll(function() {
+            var x = $(window).scrollTop();
+            if (x == 0) {
+			  $('#navbar').removeClass('navbar-mode-1').addClass('navbar-mode-2');
+            } else {
+			  $('#navbar').removeClass('navbar-mode-2').addClass('navbar-mode-1');
+            }
+        });
+    </script>
+
+	
+	
   </body>
 </html>
+
